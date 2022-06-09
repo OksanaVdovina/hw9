068b829fcfdef4afcdaf41dfd96e3c9a0d18da3f
Oksana Vdovina <oksanavdovina@inbox.ru>
2022-06-09 16:02:12 +0700

    Домашка 9 по классам

---
+++ src/hw9/Author.java
@@ -0,0 +1,21 @@
+package hw9;
+
+public class Author {
+    private String FirstName;
+    private String LastName;
+
+
+    public Author(String FirstName, String LastName) {
+        this.FirstName = FirstName;
+        this.LastName = LastName;
+    }
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+}
---
+++ src/hw9/Book.java
@@ -0,0 +1,25 @@
+package hw9;
+
+public class Book {
+    private final String nameOfTheBook;
+    private int publishingYear;
+    Author author;
+
+    public Book(String nameOfTheBook, int publishingYear, Author author) {
+        this.nameOfTheBook = nameOfTheBook;
+        this.publishingYear = publishingYear;
+        this.author = author;
+    }
+
+    public String getNameOfTheBook() {
+            return this.nameOfTheBook;
+    }
+
+    public int getPublishingYear() {
+            return publishingYear;
+    }
+    public void setPublishingYear(int publishingYear){
+        this.publishingYear = publishingYear;
+    }
+
+}
